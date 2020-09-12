9454f7d2f7fe7c15d9bf2547f7a4ba1040e4139a
Estiventh Neira <estiventhneira@gmail.com>
2020-09-12 01:32:40 -0500

    repaso de react

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,14 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Menu from "./Menu";
 import Usuarios from "./usuarios/index";
 
+const Tareas = () => <div>Tareas</div>;
+
 function App() {
   return (
     <BrowserRouter>
       <Menu />
       <Route exact path="/" component={Usuarios} />
+      <Route exact path="/tareas" component={Tareas} />
     </BrowserRouter>
   );
 }
